b60d104d2762ac68e263b604949b209e2202dd82
akhil0003 <akhil.singh0003@gmail.com>
2020-08-03 08:19:13 +1000

    Updates

---
+++ precache-manifest.8a75a23038e079cdfe46aedbc5b94733.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4365ecf8430679f0acf83b1f873de1d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c121fbec57ad099554be",
+    "url": "/portfolio/static/css/main.d090ea97.chunk.css"
+  },
+  {
+    "revision": "4cf32680843e2287d567",
+    "url": "/portfolio/static/js/2.e75a5f9b.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/portfolio/static/js/2.e75a5f9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c121fbec57ad099554be",
+    "url": "/portfolio/static/js/main.9f39d535.chunk.js"
+  },
+  {
+    "revision": "7306a7c00cc288faab5f",
+    "url": "/portfolio/static/js/runtime-main.8e326953.js"
+  },
+  {
+    "revision": "7ed36eb07742be0ec902d7335574fe7b",
+    "url": "/portfolio/static/media/Poppins-Bold.7ed36eb0.woff"
+  },
+  {
+    "revision": "709a4a4aae36d73b234295aabca0c8f1",
+    "url": "/portfolio/static/media/Raleway-Regular.709a4a4a.woff"
+  },
+  {
+    "revision": "0dbd621e4c17169bb43cb479b51e11f0",
+    "url": "/portfolio/static/media/akhil.0dbd621e.jpg"
+  },
+  {
+    "revision": "2aa5ea1c780658483a55935f719a845f",
+    "url": "/portfolio/static/media/aurainterior.2aa5ea1c.png"
+  },
+  {
+    "revision": "73b20372c6c5fd4966873fbe076c1b5f",
+    "url": "/portfolio/static/media/darkstudio.73b20372.jpg"
+  },
+  {
+    "revision": "099d3872361b173825f20fafc390a37e",
+    "url": "/portfolio/static/media/matrix.099d3872.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.e3becb3fe569c58a12ff4275839039d3.js"
+  "/portfolio/precache-manifest.8a75a23038e079cdfe46aedbc5b94733.js"
 );
 
 self.addEventListener('message', (event) => {
